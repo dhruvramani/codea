ba16b137388abc4a806388a67d04b1777a427948
Dhruv <dhruvramani98@gmail.com>
2020-12-15 12:03:31 +0530

    changed dataset dir

--- code_summ/config.py
+++ code_summ/config.py
@@ -7,8 +7,8 @@ import argparse
 
 MODULE_DIR = os.path.dirname((os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(MODULE_DIR)
-DATA_DIR = "/content/drive/My Drive/Startup/data/"
-#DATA_DIR = "/scratch/sceatch2/dhruvramani/code_data"
+DATA_DIR = "/content/drive/My Drive/Startup/datasets/"
+#DATA_DIR = "/scratch/sceatch2/dhruvramani/code_data/"
 
 TIME_STAMP = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
