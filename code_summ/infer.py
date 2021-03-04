86a2f959afd4784ac08f3cc22504af0f0544022a
Dhruv <dhruvramani98@gmail.com>
2021-03-04 15:03:37 +0530

    code_summ - shape

--- code_summ/infer.py
+++ code_summ/infer.py
@@ -3,10 +3,7 @@ import onnxruntime
 
 def infer(code, onnx_model, tokenizer):
     enc_input = tokenizer(code, return_tensors='pt')
-    inputs_onnx = {k: v.cpu().detach().numpy() for k, v in enc_input.items()}
-
     preds = run_model(onnx_model, inputs_onnx['input_ids'], inputs_onnx['attention_mask'])
-
     p = []
     for pred in preds:
         t = pred[0].cpu().numpy()
@@ -19,19 +16,31 @@ def infer(code, onnx_model, tokenizer):
     return p
 
 def run_model(model, input_ids, attention_mask):
-    ort_inputs = {'input_ids': input_ids, 'attention_mask': attention_mask} 
-    # ort_inputs = {model.get_inputs()[0].name: input_ids, model.get_inputs()[1].name: attention_mask} 
+    ort_outs = model(source_ids=input_ids, source_mask=attention_mask)
+    return ort_outs
 
-    ort_outs = model.run(None, ort_inputs)
-    return ort_outs[0]
+def get_model(encoder_path, decoder_path, dense_path, lm_path, tokenizer):
+    # onnx_model = onnx.load(path)
+    # onnx.checker.check_model(onnx_model)
+    from models import Seq2Seq
+    from transformers import RobertaConfig
 
-def get_model(path):
-    onnx_model = onnx.load(path)
-    onnx.checker.check_model(onnx_model)
+    encoder = onnxruntime.InferenceSession(encoder_path)
+    decoder = onnxruntime.InferenceSession(decoder_path)
+    dense = onnxruntime.InferenceSession(dense_path)
+    lm_head = onnxruntime.InferenceSession(lm_path)
 
-    onnx_model = onnxruntime.InferenceSession(path)
+    model_config = RobertaConfig.from_pretrained('microsoft/codebert-base')
+    model_config.output_hidden_states = True
+
+    onnx_model = Seq2Seq(encoder, decoder, model_config, onnx=True, dense=dense, lm_head=lm_head, beam_size=10, \
+            max_length=128, sos_id=tokenizer.cls_token_id, eos_id=tokenizer.sep_token_id)
 
     return onnx_model
+
+def convert_p_codebert_onnx(ckpt_dir):
+    
+
 
 # Found input input_ids with shape: {0: 'batch', 1: 'sequence'}
 # Found input attention_mask with shape: {0: 'batch', 1: 'sequence'}
