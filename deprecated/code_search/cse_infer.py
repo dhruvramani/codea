599e612b174bfa5f833b23f66cd9566eea2814cb
Dhruv <dhruvramani98@gmail.com>
2021-04-02 21:30:39 +0530

    Removed code search

--- deprecated/code_search/cse_infer.py
+++ deprecated/code_search/cse_infer.py
@@ -26,6 +26,5 @@ def get_model(path):
     return onnx_model
 
 def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
